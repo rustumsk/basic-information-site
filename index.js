3476645a39f71d42925f18e56476fa09fb9351e3
rustumsk <rustumarellano@gmail.com>
2024-07-24 21:15:13 +0800

    refactored the code using express and its get function to request the content dynamically on the server.

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+const express = require('express');
+const app = express();
+
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-http.createServer((req,res) =>{
-    const u = url.parse(req.url,true);
-    console.log("the server is running!");
-    let pathName = "." + u.pathname;
-    console.log(pathName);
+const PORT = 8080;
+let pathName = '';
+app.get('/', (req,res) =>{
 
-    if (pathName === './'){
-        pathName = './index.html';
-        console.log(pathName);
-    }
+    pathName = './index.html';
 
+    reader(res);
+});
+app.get('/about', (req,res) =>{
+
+    pathName = './about.html';
+
+    reader(res);
+});
+app.get('/contact-me', (req,res) =>{
+
+    pathName = './contact-me.html';
+
+    reader(res);
+});
+app.listen(PORT, () => 'listening');
+
+function reader(res){
     fs.readFile(pathName, (err,data) =>{
         if(err){
             fs.readFile('./404.html', (err404,errData) =>{
@@ -34,5 +49,41 @@ http.createServer((req,res) =>{
             res.end();
         }
     })
+}
+//THE COMMENTED CODE BELOW IS BY USING VANILLA JS, THE ONE ABOVE IS BY USING EXPRESS.
+
+
+// http.createServer((req,res) =>{
+//     const u = url.parse(req.url,true);
+//     console.log("the server is running!");
+//     let pathName = "." + u.pathname;
+//     console.log(pathName);
+
+//     if (pathName === './'){
+//         pathName = './index.html';
+//         console.log(pathName);
+//     }
+
+//     fs.readFile(pathName, (err,data) =>{
+//         if(err){
+//             fs.readFile('./404.html', (err404,errData) =>{
+//                 if(err404){
+//                     res.writeHead(500,{'Content-type': 'text/html'});
+//                     res.write('500 internal server error!');
+//                     res.end();
+//                 }
+//                 else{
+//                     res.writeHead(404,{'Content-type': 'text/html'});
+//                     res.write(errData);
+//                     res.end();
+//                 }
+//             })
+//         }
+//         else{
+//             res.writeHead(200,{'Content-type': 'text/html'});
+//             res.write(data);
+//             res.end();
+//         }
+//     })
     
-}).listen(8080);
+// }).listen(8080);
